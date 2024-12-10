662677d92d7a13db327fdc86b076ab67aa01dc67
Weshnaw <bfallquist@gmail.com>
2024-12-10 17:33:47 -0500

    add noise and colour

--- src/terrain_gen.rs
+++ src/terrain_gen.rs
@@ -1,5 +1,6 @@
-use bevy::{prelude::*, render::mesh::VertexAttributeValues};
+use bevy::{color::palettes::tailwind::{AMBER_800, GREEN_400}, prelude::*, render::mesh::VertexAttributeValues};
 use light_consts::lux;
+use noise::{BasicMulti, NoiseFn, Perlin};
 
 use crate::ApplicationStates;
 
@@ -28,15 +29,48 @@ fn setup_terrain(
         Transform::from_xyz(0., 20., 75.).looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
     ));
 
-    let mut terrain = Mesh::from(Plane3d::default().mesh().size(50., 50.).subdivisions(100));
+    let mut terrain = Mesh::from(
+        Plane3d::default()
+            .mesh()
+            .size(1000., 1000.)
+            .subdivisions(200),
+    );
 
     if let Some(VertexAttributeValues::Float32x3(pos)) =
         terrain.attribute_mut(Mesh::ATTRIBUTE_POSITION)
     {
+        let noise = BasicMulti::<Perlin>::new(1624);
+        let normalizer = 300.;
+        let scalar = 70.;
         for pos in pos.iter_mut() {
-            pos[1] = (pos[0] + pos[2]).sin();
+            pos[1] =
+                noise.get([pos[0] as f64 / normalizer, pos[2] as f64 / normalizer]) as f32 * scalar;
         }
+
+        let colors: Vec<[f32; 4]> = pos.iter().map(|[_, g, _]| {
+            let g = *g / scalar * 2.;
+
+            if g > 0.8 {
+                Color::LinearRgba(LinearRgba {
+                    red: 20.,
+                    green: 20.,
+                    blue: 20.,
+                    alpha: 1.,
+                })
+            } else if g > 0.3 {
+                Color::from(AMBER_800)
+            } else if g < -0.8 {
+                Color::BLACK
+            } else {
+                Color::from(GREEN_400)
+            }.to_linear().to_f32_array()
+        }).collect();
+
+        terrain.insert_attribute(Mesh::ATTRIBUTE_COLOR, colors);
+
+        terrain.compute_normals();
     }
+
 
     commands.spawn((
         Mesh3d(meshes.add(terrain)),
