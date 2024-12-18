3ee5ecdf4e9d379240b8a89a5b1d6d4899675d47
Weshnaw <bfallquist@gmail.com>
2024-12-18 11:53:36 -0500

    minimal plugins

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,3 @@
-use bevy::prelude::*;
-
-pub mod assets;
 pub mod debug;
 pub mod terrain_gen;
 pub mod camera;
-
-#[derive(Clone, Eq, PartialEq, Debug, Hash, Default, States)]
-pub(crate) enum ApplicationStates {
-    #[default]
-    AssetLoading,
-    LoadingComplete,
-}
-
-    // commands.spawn((
-    //     Name::new("Character"),
-    //     SceneRoot(
-    //         player.player.clone()
-    //     )
-    // ));
-
-    // commands.spawn((
-    //     Name::new("Level"),
-    //     SceneRoot(
-    //         levels.level_01.clone()
-    //     )
-    // ));
